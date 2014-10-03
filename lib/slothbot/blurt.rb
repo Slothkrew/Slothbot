a57249e3e65a7f9a1092ffeda7f73bdbbf2546de
parametaortho <s@shaunwheelhou.se>
2014-10-04 03:34:14 +1000

    durp

--- lib/slothbot/blurt.rb
+++ lib/slothbot/blurt.rb
@@ -2,16 +2,56 @@
 require 'cinch'
 
 module Slothbot
+
   module Plugins
 		
     ##
     # A plugin that blurts out a random configured phrase every five to ten
     # minutes.
 
-    class Blurt < Cinch::Plugin
-      # TODO
+    class Blurt
+
+      include Cinch::Plugin
+
+      match "blurt"
+      listen_to :blurt, :method => :blurt
+
+      def blurt
+        if config[:phrases]
+          @bot.channels.each do |channel|
+            Channel(channel).send(
+              config[:phrases][Random.rand(config[:phrases].length)]
+            )
+          end
+        else
+          debug @@no_phrases
+        end
+      end
+
+      def execute(message)
+        blurt
+      end
+
+      def initialize(*)
+        super
+        if config[:phrases]
+          Thread.new do |t|
+            loop do
+              blurt
+              sleep Random.rand(60*5) + 60*5
+            end
+          end
+        else
+          debug @@no_phrases
+        end
+      end
+
+    private
+      @@no_phrases = "Phrases are not configured (:phrases). Cannot blurt."
+
     end
 
   end
+
 end
 
