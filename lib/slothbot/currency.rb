15587bac4a6962dc3bae006d203e84d91fed0caa
Nikolaj <sjums07@gmail.com>
2015-07-23 10:39:03 +0200

    yes, this should work (pls pls pls pls)

--- lib/slothbot/currency.rb
+++ lib/slothbot/currency.rb
@@ -17,12 +17,13 @@ module Slothbot
         currency_b = :usd
       end
       c_a, c_b = currency_a.to_s.upcase, currency_b.to_s.upcase
-      pair_key = "#{c_a}_#{c_b}"
+      #pair_key = "#{c_a}_#{c_b}"
       queries = @queries.map { |k,v| "#{k}=#{v}" }.join '&'
-      queries.sub! '{currency}', pair_key
+      queries.sub! '{c_a}', c_a
+      queries.sub! '{c_b}', c_b
       response = Net::HTTP.get @fqdn, "#{@resource}?#{queries}"
       if response.strip != '{}'
-        value = JSON.parse(response)[pair_key]['val'].to_f * quantity
+        value = JSON.parse(response)['rates'][c_b].to_f * quantity
         value = value * @sloth_multiplier if sloth_mode == :from
         value = value / @sloth_multiplier if sloth_mode == :to
         return value.round 3
@@ -32,9 +33,9 @@ module Slothbot
     end
 
     def initialize
-      @fqdn = 'www.freecurrencyconverterapi.com'
-      @resource = '/api/v2/convert'
-      @queries = { 'q' => '{currency}', 'compact' => 'y' }
+      @fqdn = 'http://api.fixer.io'
+      @resource = '/latest'
+      @queries = { 'base' => '{c_a}',  'symbols' => '{c_b}'}
       @sloth_multiplier = 2100
     end
   end
